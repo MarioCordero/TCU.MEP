878d3b4e369ef9f16f4342b431e7a950e5a6ed5e
Mario Cordero <mariogabriel.cordero@ucr.ac.cr>
2025-09-27 21:10:28 -0600

    # FEAT/REFRACT - Agregado posibilidad de credenciales en el DB Handler

--- API/dbhandler.php
+++ API/dbhandler.php
@@ -1,8 +1,15 @@
 <?php
-    $host = "localhost";
-    $user = "mario";
-    $password = "2003";
-    $dbname = "ChemMaster";
+    // Load configuration from config.ini
+    $config = parse_ini_file('config.ini', true);
+    
+    if (!$config) {
+        die(json_encode(["success" => false, "message" => "Error loading configuration file"]));
+    }
+    
+    $host = $config['database']['host'];
+    $user = $config['database']['user'];
+    $password = $config['database']['password'];
+    $dbname = $config['database']['dbname'];
 
     $conn = new mysqli($host, $user, $password, $dbname);
 
